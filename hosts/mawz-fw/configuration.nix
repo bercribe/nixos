12945a65d0bdd8e60ba8b8c7720ac79ef81b2337
mawz <mawz@hey.com>
2024-05-10 18:26:35 -0700

    mawz-fw: 30 current  2024-05-10 18:26:31  23.11.6981.27c13997bf45  6.1.90                          *

--- hosts/mawz-fw/configuration.nix
+++ hosts/mawz-fw/configuration.nix
@@ -13,6 +13,7 @@
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
