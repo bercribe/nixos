a5748d5e2047bd5e1bbbb29ffcda1a7de4fa223f
mawz <mawz@hey.com>
2024-05-18 16:53:30 -0700

    mawz-fw: 63 current  2024-05-17 21:28:24  24.05.20240512.2057814   6.6.30                          *

--- hosts/mawz-fw/configuration.nix
+++ hosts/mawz-fw/configuration.nix
@@ -109,9 +109,9 @@
   # };
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
 
   # Enable CUPS to print documents.
