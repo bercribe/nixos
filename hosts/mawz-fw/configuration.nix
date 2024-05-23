1dec9b9dfc4689751b4e0333b856b1937ce3b0c0
mawz <mawz@hey.com>
2024-05-22 20:41:16 -0700

    mawz-fw: 92 current  2024-05-22 20:28:05  24.05.20240512.2057814   6.6.30                          *

--- hosts/mawz-fw/configuration.nix
+++ hosts/mawz-fw/configuration.nix
@@ -103,39 +103,9 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Change DPI
-  # services.xserver.dpi = 160;
-
   # Enable the GNOME Desktop Environment.
   # services.xserver.displayManager.gdm.enable = true;
   # services.xserver.desktopManager.gnome.enable = true;
-
-  # Tiling window manager
-  # services.xserver = {
-  #   desktopManager = {
-  #     xterm.enable = false;
-  #     xfce = {
-  #       enable = true;
-  #       noDesktop = true;
-  #       enableXfwm = false;
-  #     };
-  #   };
-
-  #   displayManager = {
-  #     lightdm.enable = true;
-  #     defaultSession = "xfce+i3";
-  #   };
-
-  #   windowManager.i3 = {
-  #     enable = true;
-  #     extraPackages = with pkgs; [
-  #       dmenu #application launcher most people use
-  #       i3status # gives you the default i3 status bar
-  #       i3lock #default i3 screen locker
-  #       # i3blocks #if you are planning on using i3blocks over i3status
-  #     ];
-  #   };
-  # };
 
   # Configure keymap in X11
   services.xserver.xkb = {
