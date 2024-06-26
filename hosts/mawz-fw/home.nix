45742516f69ce8a45bf6a11d28460488d0e44c39
mawz <mawz@hey.com>
2024-06-25 22:33:02 -0700

    mawz-fw: 220 current  2024-06-25 22:46:46  24.11.20240524.bfb7a88   6.6.31                          *

--- hosts/mawz-fw/home.nix
+++ hosts/mawz-fw/home.nix
@@ -57,6 +57,20 @@
     keybindings = {
       "<c-c>" = "copy-path";
     };
+    previewer.source = pkgs.writeShellScript "pv.sh" ''
+      #!/bin/sh
+
+      shopt -s nocasematch
+      case "$1" in
+          *.jpg) ${pkgs.exiftool}/bin/exiftool "$1";;
+          *.tar*) tar tf "$1";;
+          *.zip) unzip -l "$1";;
+          *.rar) unrar l "$1";;
+          *.7z) 7z l "$1";;
+          *.pdf) pdftotext "$1" -;;
+          *) highlight -O ansi "$1" || cat "$1";;
+      esac
+    '';
   };
 
   # This value determines the Home Manager release that your
