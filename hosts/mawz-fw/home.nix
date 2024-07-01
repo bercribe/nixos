1ecad4a311fc520df87e0dea95ecb3282e83632b
mawz <mawz@hey.com>
2024-06-30 20:21:44 -0700

    mawz-fw: 236 current  2024-06-30 20:21:29  24.11.20240524.bfb7a88   6.6.31                          *

--- hosts/mawz-fw/home.nix
+++ hosts/mawz-fw/home.nix
@@ -67,7 +67,7 @@
 
       shopt -s nocasematch
       case "$1" in
-          *.gif | *.jpg | *.jpeg | *.mov | *.mpg | *.mp4 | *.pcx | *.png | *.psd | *.wav)
+          *.avi | *.gif | *.jpg | *.jpeg | *.mov | *.mpg | *.mp4 | *.pcx | *.png | *.psd | *.thm | *.wav)
               ${pkgs.exiftool}/bin/exiftool -S -DateTimeOriginal -MediaCreateDate -FileModifyDate "$1";
               echo "--------------------------------"; ${pkgs.exiftool}/bin/exiftool "$1";;
           *.tar*) tar tf "$1";;
