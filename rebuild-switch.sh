3539053042a69d5fcbb99bca22669490a30413af
mawz <mawz@hey.com>
2024-07-27 14:35:25 -0700

    add force arg to script

--- rebuild-switch.sh
+++ rebuild-switch.sh
@@ -3,15 +3,52 @@
 set -e
 
 # set up args
-if [ "$1" == "-t" ]; then
-    disableCommit=true
+# ignore errexit with `&& true`
+getopt --test > /dev/null && true
+if [[ $? -ne 4 ]]; then
+    echo 'I’m sorry, `getopt --test` failed in this environment.'
+    exit 1
 fi
+
+LONGOPTS=test,force
+OPTIONS=tf
+
+# -temporarily store output to be able to check for errors
+# -activate quoting/enhanced mode (e.g. by writing out “--options”)
+# -pass arguments only via   -- "$@"   to separate them correctly
+# -if getopt fails, it complains itself to stdout
+PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@") || exit 2
+# read getopt’s output this way to handle the quoting right:
+eval set -- "$PARSED"
+
+disableCommit=false forceRun=false
+# now enjoy the options in order and nicely split until we see --
+while true; do
+    case "$1" in
+        -t|--test)
+            disableCommit=true
+            shift
+            ;;
+        -f|--force)
+            forceRun=true
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Programming error"
+            exit 1
+            ;;
+    esac
+done
 
 # cd to your config dir
 pushd $(dirname "$0")
 
 # Early return if no changes were detected (thanks @singiamtel!)
-if git diff HEAD --quiet '*.nix'; then
+if [ "$forceRun" != true ] && git diff HEAD --quiet '*.nix'; then
     echo "No changes detected, exiting."
     popd
     exit 0
