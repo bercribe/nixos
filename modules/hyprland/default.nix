6f143f13d806c5c330cca9bdb9ef458568e54329
mawz <mawz@hey.com>
2024-07-27 17:04:19 -0700

    mawz-hue: 35 current  2024-07-27 17:04:14  24.11.20240703.9f4128e   6.6.36                          *

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -40,6 +40,10 @@
     libsForQt5.qt5.qtgraphicaleffects
   ];
 
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+
   # icons for waybar
   fonts.packages = with pkgs; [font-awesome];
 
