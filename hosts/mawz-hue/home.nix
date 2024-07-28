f71a9d5850bdff87515fef8e9ab90bf16c6af80a
mawz <mawz@hey.com>
2024-07-27 17:14:18 -0700

    mawz-hue: 37 current  2024-07-27 17:14:13  24.11.20240703.9f4128e   6.6.36                          *

--- hosts/mawz-hue/home.nix
+++ hosts/mawz-hue/home.nix
@@ -5,8 +5,8 @@
 }: {
   wayland.windowManager.hyprland.settings = {
     monitor = lib.mkForce [
-      "desc:Ancor Communications Inc ROG PG279Q ##ASNDQNtIJcHd,preferred,0x0,1"
-      "desc:Ancor Communications Inc ROG PG279Q ##ASNpS7wVCX7d,preferred,2560x-900,1,transform,1"
+      "desc:Ancor Communications Inc ROG PG279Q ##ASNDQNtIJcHd,2560x1440@144,0x0,1"
+      "desc:Ancor Communications Inc ROG PG279Q ##ASNpS7wVCX7d,2560x1440@144,2560x-900,1,transform,1"
     ];
     cursor = {
       no_hardware_cursors = true;
