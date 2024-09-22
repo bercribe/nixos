ce72025106a8144c460b0fe4a3012500aeaaeb76
mawz <mawz@hey.com>
2024-09-21 23:16:35 -0700

    mawz-hue: 245 current  2024-09-21 23:44:40  24.05.20240916.086b448   6.6.51                          *

--- modules/systems/desktop/default.nix
+++ modules/systems/desktop/default.nix
@@ -357,6 +357,11 @@
     image = ./wallpaper.jpg;
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
+    cursor = {
+      package = pkgs.rose-pine-cursor;
+      name = "BreezeX-RosePineDawn-Linux";
+      size = 32;
+    };
   };
 
   # List services that you want to enable:
