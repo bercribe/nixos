a1b702d56ec4142504ab81fd35b7079442dad3e8
mawz <mawz@hey.com>
2024-09-29 22:20:05 -0700

    mawz-nuc: 137 current  2024-09-29 22:19:56  24.05.20240916.086b448   6.6.51                          *

--- modules/systems/network/ssh.nix
+++ modules/systems/network/ssh.nix
@@ -3,6 +3,14 @@
   config,
   ...
 }: {
+  imports = [(self + /modules/sops.nix)];
+
+  sops.secrets.ssh = {
+    owner = "mawz";
+    path = "/home/mawz/.ssh/id_ed25519";
+    key = "${config.networking.hostName}/ssh";
+  };
+
   programs.ssh.knownHosts = {
     mawz-nas = {
       extraHostNames = ["192.168.0.43"];
