b21d6e8ed214d86fb4937aba0739ad063297e2df
mawz <mawz@hey.com>
2024-10-05 19:32:01 -0700

    mawz-vault: 77 current  2024-10-05 19:31:53  24.05.20240916.086b448   6.6.51                          *

---
+++ modules/clients/healthchecks-emails.nix
@@ -0,0 +1,32 @@
+{
+  self,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    (self + /modules/sops.nix)
+    (self + /modules/services/postfix.nix)
+  ];
+
+  sops.secrets."healthchecks/remote/email-receiver" = {};
+
+  systemd.timers.email-heartbeat = {
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      OnBootSec = "1h";
+      OnUnitActiveSec = "1h";
+      Unit = "uptime-heartbeat.service";
+    };
+  };
+  systemd.services.email-heartbeat = {
+    script = ''
+      receiver="$(cat ${config.sops.secrets."healthchecks/remote/email-receiver".path})"
+      ${pkgs.mailutils}/bin/mail -s "Email notifs good!" $receiver
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+  };
+}
--- modules/services/monitoring/default.nix
+++ modules/services/monitoring/default.nix
@@ -3,5 +3,6 @@
     ./healthchecks.nix
     ./uptime-kuma.nix
     (self + /modules/clients/healthchecks-heartbeats.nix)
+    (self + /modules/clients/healthchecks-emails.nix)
   ];
 }
