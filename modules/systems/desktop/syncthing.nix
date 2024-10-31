ff9166b3afa98698ce80fd2e168b736859e391b4
mawz <mawz@hey.com>
2024-10-31 16:23:17 +0800

    mawz-fw: 109 current  2024-10-31 16:23:08  24.05.20240916.086b448   6.6.51                          *

--- modules/systems/desktop/syncthing.nix
+++ modules/systems/desktop/syncthing.nix
@@ -23,6 +23,7 @@
   services.syncthing = {
     enable = true;
     user = "mawz";
+    openDefaultPorts = true;
     dataDir = "/home/mawz/Documents"; # Default folder for new synced folders
     configDir = "/home/mawz/Documents/.config/syncthing"; # Folder for Syncthing's settings and keys
     overrideDevices = true; # overrides any devices added or deleted through the WebUI
