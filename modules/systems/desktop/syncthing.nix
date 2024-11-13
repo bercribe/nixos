007b77337e2f7c29057fabc3667393040913ccd3
mawz <mawz@hey.com>
2024-11-13 09:35:29 -0800

    mawz-hue: 84 current  2024-11-13 09:35:23  24.05.20241030.080166c   6.6.58                          *

--- modules/systems/desktop/syncthing.nix
+++ modules/systems/desktop/syncthing.nix
@@ -24,8 +24,6 @@
     enable = true;
     user = "mawz";
     openDefaultPorts = true;
-    dataDir = "/home/mawz/Documents"; # Default folder for new synced folders
-    configDir = "/home/mawz/Documents/.config/syncthing"; # Folder for Syncthing's settings and keys
     overrideDevices = true; # overrides any devices added or deleted through the WebUI
     overrideFolders = true; # overrides any folders added or deleted through the WebUI
     cert = config.sops.secrets.syncthing-cert.path;
