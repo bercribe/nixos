dc2d6bce57cc23e36fbca0d307e6ae0cb89a3097
mawz <mawz@hey.com>
2024-11-13 19:12:57 -0800

    change hyprland monitor layout
    mawz-hue: 85 current  2024-11-13 19:12:54  24.05.20241030.080166c   6.6.58                          *

--- hosts/mawz-hue/home.nix
+++ hosts/mawz-hue/home.nix
@@ -4,27 +4,27 @@
   ...
 }: {
   wayland.windowManager.hyprland.settings = let
-    left = "desc:Ancor Communications Inc ROG PG279Q ##ASNDQNtIJcHd";
-    right = "desc:Ancor Communications Inc ROG PG279Q ##ASNpS7wVCX7d";
+    bottom = "desc:Ancor Communications Inc ROG PG279Q ##ASNpS7wVCX7d";
+    top = "desc:Ancor Communications Inc ROG PG279Q ##ASNDQNtIJcHd";
   in {
     monitor = lib.mkForce [
-      "${left},2560x1440@144,0x0,1"
-      "${right},2560x1440@144,2560x-900,1,transform,1"
+      "${bottom},2560x1440@144,0x0,1"
+      "${top},2560x1440@144,0x-1440,1"
     ];
     cursor = {
       no_hardware_cursors = true;
     };
     workspace = [
-      "1,monitor:${left},default:true"
-      "2,monitor:${left}"
-      "3,monitor:${left}"
-      "4,monitor:${left}"
-      "5,monitor:${left}"
-      "6,monitor:${right},default:true"
-      "7,monitor:${right}"
-      "8,monitor:${right}"
-      "9:monitor:${right}"
-      "10,monitor:${right}"
+      "1,monitor:${bottom},default:true"
+      "2,monitor:${bottom}"
+      "3,monitor:${bottom}"
+      "4,monitor:${bottom}"
+      "5,monitor:${bottom}"
+      "6,monitor:${top},default:true"
+      "7,monitor:${top}"
+      "8,monitor:${top}"
+      "9:monitor:${top}"
+      "10,monitor:${top}"
     ];
     exec-once = [
       "[workspace 1 silent] alacritty"
