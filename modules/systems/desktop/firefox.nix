5f476d393abe4769fc2d6576dea718320f5e1352
mawz <mawz@hey.com>
2024-11-23 14:49:22 -0800

    mawz-hue: 92 current  2024-11-23 14:49:18  24.05.20240916.086b448   6.6.51                          *

--- modules/systems/desktop/firefox.nix
+++ modules/systems/desktop/firefox.nix
@@ -13,76 +13,79 @@
       DisplayBookmarksToolbar = "always";
       # found at about:support
       # details here https://mozilla.github.io/policy-templates/#extensionsettings
-      ExtensionSettings = {
+      ExtensionSettings = let
+        mozillaAddon = id: "https://addons.mozilla.org/firefox/downloads/latest/${id}/latest.xpi";
+        installation_mode = "normal_installed";
+      in {
         "firefox@betterttv.net" = {
-          "installation_mode" = "normal_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/betterttv/latest.xpi";
-          "default_area" = "menupanel";
+          inherit installation_mode;
+          install_url = mozillaAddon "betterttv";
+          default_area = "menupanel";
         };
         "addon@darkreader.org" = {
-          "installation_mode" = "normal_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
-          "default_area" = "navbar";
+          inherit installation_mode;
+          install_url = mozillaAddon "darkreader";
+          default_area = "navbar";
         };
         "enhancerforyoutube@maximerf.addons.mozilla.org" = {
-          "installation_mode" = "normal_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/enhancer-for-youtube/latest.xpi";
-          "default_area" = "menupanel";
+          inherit installation_mode;
+          install_url = mozillaAddon "enhancer-for-youtube";
+          default_area = "menupanel";
         };
         "jid1-KKzOGWgsW3Ao4Q@jetpack" = {
-          "installation_mode" = "normal_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/i-dont-care-about-cookies/latest.xpi";
-          "default_area" = "menupanel";
+          inherit installation_mode;
+          install_url = mozillaAddon "i-dont-care-about-cookies";
+          default_area = "menupanel";
         };
         "search@kagi.com" = {
-          "installation_mode" = "normal_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/kagi-search-for-firefox/latest.xpi";
-          "default_area" = "menupanel";
+          inherit installation_mode;
+          install_url = mozillaAddon "kagi-search-for-firefox";
+          default_area = "menupanel";
         };
         "keepassxc-browser@keepassxc.org" = {
-          "installation_mode" = "normal_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/keepassxc-browser/latest.xpi";
-          "default_area" = "menupanel";
+          inherit installation_mode;
+          install_url = mozillaAddon "keepassxc-browser";
+          default_area = "menupanel";
         };
         "clipper@obsidian.md" = {
-          "installation_mode" = "normal_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/web-clipper-obsidian/latest.xpi";
-          "default_area" = "navbar";
+          inherit installation_mode;
+          install_url = mozillaAddon "web-clipper-obsidian";
+          default_area = "navbar";
         };
         "{9063c2e9-e07c-4c2c-9646-cfe7ca8d0498}" = {
-          "installation_mode" = "normal_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/old-reddit-redirect/latest.xpi";
-          "default_area" = "menupanel";
+          inherit installation_mode;
+          install_url = mozillaAddon "old-reddit-redirect";
+          default_area = "menupanel";
         };
         "jid0-adyhmvsP91nUO8pRv0Mn2VKeB84@jetpack" = {
-          "installation_mode" = "normal_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/raindropio/latest.xpi";
-          "default_area" = "navbar";
+          inherit installation_mode;
+          install_url = mozillaAddon "raindropio";
+          default_area = "navbar";
         };
         "jid1-xUfzOsOFlzSOXg@jetpack" = {
-          "installation_mode" = "normal_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/reddit-enhancement-suite/latest.xpi";
-          "default_area" = "menupanel";
+          inherit installation_mode;
+          install_url = mozillaAddon "reddit-enhancement-suite";
+          default_area = "menupanel";
         };
         "sponsorBlocker@ajay.app" = {
-          "installation_mode" = "normal_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
-          "default_area" = "menupanel";
+          inherit installation_mode;
+          install_url = mozillaAddon "sponsorblock";
+          default_area = "menupanel";
         };
         "treestyletab@piro.sakura.ne.jp" = {
-          "installation_mode" = "normal_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/tree-style-tab/latest.xpi";
-          "default_area" = "menupanel";
+          inherit installation_mode;
+          install_url = mozillaAddon "tree-style-tab";
+          default_area = "menupanel";
         };
         "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
-          "installation_mode" = "normal_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
-          "default_area" = "navbar";
+          inherit installation_mode;
+          install_url = mozillaAddon "vimium-ff";
+          default_area = "navbar";
         };
         "uBlock0@raymondhill.net" = {
-          "installation_mode" = "normal_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          "default_area" = "navbar";
+          inherit installation_mode;
+          install_url = mozillaAddon "ublock-origin";
+          default_area = "navbar";
         };
       };
     };
