ce50e8e214574bfbccd5e96399f12b30c2924c1d
mawz <mawz@hey.com>
2025-01-04 19:06:24 -0800

    heavens-door: 168 current  2025-01-04 19:06:16  24.11.20241210.a0f3e10   6.6.63                          *

--- modules/scripts/asw.nix
+++ modules/scripts/asw.nix
@@ -12,7 +12,7 @@ pkgs.writeShellScriptBin "asw" ''
           ${pkgs.pulseaudio}/bin/pactl set-default-source "alsa_input.usb-C-Media_Electronics_Inc._USB_PnP_Sound_Device-00.iec958-stereo"
   elif [[ $1 = 'i' ]] # Index
   then
-          ${pkgs.pulseaudio}/bin/pactl set-default-sink "alsa_output.pci-0000_0b_00.1.hdmi-stereo-extra2"
+          ${pkgs.pulseaudio}/bin/pactl set-default-sink "alsa_output.pci-0000_0a_00.1.hdmi-stereo-extra2"
           ${pkgs.pulseaudio}/bin/pactl set-default-source "alsa_input.usb-Valve_Corporation_Valve_VR_Radio___HMD_Mic_C84527EC3C-LYM-01.mono-fallback"
   else
           echo "\`asw h\` for headset, \`asw i\` for Index"
