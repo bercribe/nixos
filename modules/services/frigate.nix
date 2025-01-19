69c5d527c60f9ccc7108f74897d2d5c0cf9b8cf5
mawz <mawz@hey.com>
2025-01-18 22:56:30 -0800

    init frigate module

---
+++ modules/services/frigate.nix
@@ -0,0 +1,110 @@
+{
+  self,
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  # TODO: back this up somehow
+  dataDir = "/var/lib/frigate";
+  # TODO: frigate.lan
+  hostname = "localhost";
+  interface = "enp6s0";
+
+  cameras = {
+    mawz_office = {
+      address = "10.0.40.10";
+    };
+    front_porch = {
+      address = "10.0.40.11";
+    };
+  };
+  cameraPath = params: "rtsp://${params.userPass}@${cameras."${params.camera}".address}:554/cam/realmonitor?channel=1&subtype=${params.subtype}";
+  go2rtcStreams = userPass:
+    lib.mapAttrs (camera: value:
+      cameraPath {
+        inherit camera userPass;
+        subtype = "0";
+      })
+    cameras;
+in {
+  sops.secrets."frigate/env" = {
+    # TODO: networking.hostName
+    sopsFile = self + /secrets/moody-blues.yaml;
+  };
+
+  # camera subnet
+  networking.interfaces.${interface} = {
+    ipv4.addresses = [
+      {
+        address = "10.0.40.1";
+        prefixLength = 24;
+      }
+    ];
+  };
+
+  # hardware accel
+  hardware.graphics = {
+    enable = true;
+    # TODO: enable?
+    # extraPackages = [pkgs.intel-media-driver];
+  };
+
+  # coral TPU
+  hardware.coral.usb.enable = true;
+
+  services.frigate = {
+    enable = true;
+    inherit hostname;
+    settings = let
+      userPass = "{FRIGATE_RTSP_USER}:{FRIGATE_RTSP_PASSWORD}";
+      frigatePath = camera: subtype: (cameraPath {inherit userPass camera subtype;});
+    in {
+      mqtt = {
+        enabled = true;
+        host = "192.168.0.43";
+        user = "{FRIGATE_MQTT_USER}";
+        password = "{FRIGATE_MQTT_PASSWORD}";
+      };
+      # TODO: enable
+      # ffmpeg.hwaccel_args = "preset-vaapi";
+      detectors.coral = {
+        type = "edgetpu";
+        device = "usb";
+      };
+      objects.track = ["person" "cat" "dog"];
+      cameras =
+        lib.mapAttrs (camera: value: {
+          enabled = true;
+          ffmpeg = {
+            inputs = [
+              {
+                path = frigatePath "${camera}" "1";
+                roles = ["detect"];
+              }
+              {
+                path = frigatePath "${camera}" "0";
+                roles = ["record"];
+              }
+            ];
+          };
+          detect = {
+            enabled = true;
+            width = 704;
+            height = 480;
+          };
+          record.enabled = true;
+          snapshots.enabled = true;
+        })
+        cameras;
+      go2rtc.streams = go2rtcStreams userPass;
+    };
+  };
+  systemd.services.frigate.serviceConfig.EnvironmentFile = config.sops.secrets."frigate/env".path;
+
+  services.go2rtc = {
+    enable = true;
+    settings.streams = go2rtcStreams "\${FRIGATE_RTSP_USER}:\${FRIGATE_RTSP_PASSWORD}";
+  };
+  systemd.services.go2rtc.serviceConfig.EnvironmentFile = config.sops.secrets."frigate/env".path;
+}
