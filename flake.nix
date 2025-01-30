55f43e73eb3054f8ec6c4bc395d0d725de60d9a2
mawz <mawz@hey.com>
2025-01-29 19:36:58 -0800

    heavens-door: 235 current  2025-01-29 19:36:47  24.11.20241210.a0f3e10   6.6.63                          *

--- flake.nix
+++ flake.nix
@@ -9,11 +9,12 @@
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
 
     sops-nix.url = "github:Mic92/sops-nix";
     stylix.url = "github:danth/stylix/release-24.11";
