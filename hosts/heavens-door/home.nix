63d48b85294fd8f229d706d1cc6dc150eeabad14
mawz <mawz@hey.com>
2025-02-22 23:06:57 -0800

    heavens-door: 278 current  2025-02-22 23:06:43  24.11.20250215.a60651b   6.6.76                          *

--- hosts/heavens-door/home.nix
+++ hosts/heavens-door/home.nix
@@ -9,7 +9,7 @@
   in {
     monitor = lib.mkForce [
       "${main}, highrr, 0x0, 1"
-      "${top}, 2560x1440@100, 440x-1440, 1"
+      "${top}, highrr, 440x-1440, 1"
     ];
     cursor = {
       no_hardware_cursors = true;
