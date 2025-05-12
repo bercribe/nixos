be0af0ab4dddd6dbdc2daed08771342f181f3a25
mawz <mawz@hey.com>
2025-05-11 18:10:37 -0700

    monitoring dashboard
    judgement: 219 current  2025-05-11 18:50:23  24.11.20250215.a60651b   6.6.76                          *

--- modules/services/homepage.nix
+++ modules/services/homepage.nix
@@ -1,4 +1,5 @@
 {
+  self,
   config,
   lib,
   local,
@@ -11,23 +12,48 @@ in {
   options.local.services.homepage-dashboard.enable = lib.mkEnableOption "homepage-dashboard";
 
   config = lib.mkIf cfg.enable {
+    sops.secrets."healthchecks/local/api-key-ro" = {
+      sopsFile = self + /secrets/common.yaml;
+    };
+    sops.secrets."healthchecks/remote/api-key-ro" = {
+      sopsFile = self + /secrets/common.yaml;
+    };
     # TODO: use `allowedHosts` option after update
     sops.templates."homepage.env".content = ''
       HOMEPAGE_ALLOWED_HOSTS=${utils.localHostUrlBase "homepage-dashboard"}
+      HOMEPAGE_VAR_HEALTHCHECKS_LOCAL_API_KEY=${config.sops.placeholder."healthchecks/local/api-key-ro"}
+      HOMEPAGE_VAR_HEALTHCHECKS_REMOTE_API_KEY=${config.sops.placeholder."healthchecks/remote/api-key-ro"}
     '';
 
     services.homepage-dashboard = {
       enable = true;
       listenPort = port;
       services = let
+        serviceEndpoint = service: let
+          registryEntry = config.local.service-registry."${service}";
+          shortName = registryEntry.shortName;
+          host = lib.head registryEntry.hosts;
+        in "https://${shortName}.${host}.mawz.dev";
+
         icons = {
           paisa = "https://paisa.fyi/images/logo.svg";
+        };
+        widgets = {
+          gatus = {
+            type = "gatus";
+            url = serviceEndpoint "gatus";
+          };
+          healthchecks = {
+            type = "healthchecks";
+            url = serviceEndpoint "healthchecks";
+            key = "{{HOMEPAGE_VAR_HEALTHCHECKS_LOCAL_API_KEY}}";
+          };
         };
 
         serviceName = service: lib.defaultTo service config.local.service-registry."${service}".friendlyName;
         mkService = {
           service,
-          host,
+          host ? lib.head config.local.service-registry."${service}".hosts,
         }: let
           shortName = config.local.service-registry."${service}".shortName;
           name = serviceName service;
@@ -36,6 +62,7 @@ in {
             href = "https://${shortName}.${host}.mawz.dev";
             icon = icons."${service}" or name;
             description = "Running on ${host}";
+            widget = lib.mkIf (widgets ? "${service}") widgets."${service}";
           };
         };
 
@@ -50,16 +77,36 @@ in {
             then
               mkService {
                 inherit service;
-                host = head hosts;
               }
             else {
               "${serviceName service}" =
                 hosts
                 |> map (host: mkService {inherit service host;});
             });
+
+        monitoringServices = [
+          (mkService {service = "gatus";})
+          (mkService {service = "healthchecks";})
+          {
+            "upstream healthchecks" = let
+              url = "https://healthchecks.io/";
+            in {
+              href = url;
+              icon = "healthchecks";
+              widget = {
+                type = "healthchecks";
+                inherit url;
+                key = "{{HOMEPAGE_VAR_HEALTHCHECKS_REMOTE_API_KEY}}";
+              };
+            };
+          }
+        ];
       in [
         {
           Homelab = homelabServices;
+        }
+        {
+          Monitoring = monitoringServices;
         }
       ];
       environmentFile = config.sops.templates."homepage.env".path;
