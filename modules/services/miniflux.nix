9961d4a57a4929521246a6f2c8d9397889fccda3
mawz <mawz@hey.com>
2025-08-28 20:08:58 -0700

    judgement: 323 current  2025-08-28 20:13:07  25.05.20250522.55d1f92   6.12.30                          *

--- modules/services/miniflux.nix
+++ modules/services/miniflux.nix
@@ -30,7 +30,6 @@ in {
       adminCredentialsFile = config.sops.secrets.miniflux-admin.path;
     };
 
-    networking.hosts."127.0.0.1" = [rssBridgeHost];
     services.rss-bridge = {
       enable = true;
       webserver = "caddy";
