4ba446d5ae6624e648809aa5746ba3dfd87a74c4
mawz <mawz@hey.com>
2025-09-01 18:00:38 -0700

    factor out vim config

--- modules/systems/home/minimal.nix
+++ modules/systems/home/minimal.nix
@@ -6,6 +6,8 @@
 }: let
   cfg = config.local;
 in {
+  imports = [./vim.nix];
+
   options.local.yazi = with lib;
   with types; {
     keybinds = mkOption {
@@ -52,33 +54,6 @@ in {
       extraConfig = ''
         # fix warnings caused by UWSM
         set -g default-command "''${SHELL}"
-      '';
-    };
-
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
-      plugins = with pkgs.vimPlugins; [
-        telescope-nvim
-        telescope-fzf-native-nvim
-        neo-tree-nvim
-        nvim-web-devicons
-      ];
-      extraConfig = ''
-        " Fix tabs
-        set tabstop=2 shiftwidth=2 smarttab
-        " Use spaces instead of tabs
-        set expandtab
-        " Use system clipboard
-        " set clipboard=unnamedplus
-        " Find files using Telescope command-line sugar.
-        nnoremap <leader>ff <cmd>Telescope find_files<cr>
-        nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-        nnoremap <leader>fb <cmd>Telescope buffers<cr>
-        nnoremap <leader>fh <cmd>Telescope help_tags<cr>
-        " Show file tree
-        nnoremap <leader>tr :Neotree reveal right<cr>
-        nnoremap <leader>tt :Neotree toggle right<cr>
       '';
     };
 
