61eb0a8fbb2a00cf8548dc49691e78f60c01dbef
mawz <mawz@hey.com>
2025-09-16 13:42:30 -0700

    make lsp + treesitter optionsn available
    highway-star: 35 current  2025-09-16 13:42:26  25.05.20250522.55d1f92  6.12.30                          *

--- modules/systems/home/vim.nix
+++ modules/systems/home/vim.nix
@@ -1,81 +1,95 @@
 {
+  config,
   pkgs,
   lib,
   ...
-}: {
-  programs.neovim = let
-    languageServers = with pkgs; {
-      clangd = libclang;
-      lua_ls = lua-language-server;
-      nixd = nixd;
-      pyright = pyright;
-      tinymist = tinymist;
+}: let
+  cfg = config.local.vim;
+in {
+  options.local.vim = with lib;
+  with types; {
+    languageServers = mkOption {
+      type = attrsOf package;
+      description = "Language servers to use. Key is the name of the LSP in nvim-lspconfig, value is the package.";
+      default = with pkgs; {
+        clangd = libclang;
+        lua_ls = lua-language-server;
+        nixd = nixd;
+        pyright = pyright;
+        tinymist = tinymist;
+      };
     };
-    treesitterSyntaxes =
-      pkgs.vimPlugins.nvim-treesitter.withPlugins
-      (p: with p; [lua nix python typst]);
-  in {
-    enable = true;
-    defaultEditor = true;
-    plugins = let
-      toLua = str: "lua << EOF\n${str}\nEOF\n";
-    in
-      with pkgs.vimPlugins; [
-        fzf-lua # quick opener w/ fzf
-        nvim-lspconfig # language servers
-        typst-preview-nvim # live preview for typst
-        yazi-nvim # file picker
-        {
-          plugin = blink-cmp; # auto completion
-          config = toLua ''
-            require('blink.cmp').setup({
-              -- C-y to accept
-              -- C-space: Open menu or open docs if already open
-              -- C-n/C-p or Up/Down: Select next/previous item
-              -- C-e: Hide menu
-              -- C-k: Toggle signature help (if signature.enabled = true)
-              keymap = { preset = 'default' },
+    treesitterParsers = mkOption {
+      type = listOf package;
+      description = "Treesitter parsers to use";
+      default = with pkgs.vimPlugins.nvim-treesitter-parsers; [lua nix python typst];
+    };
+  };
 
-              appearance = {
-                nerd_font_variant = 'mono'
-              },
-              completion = { documentation = { auto_show = false } },
-              sources = {
-                default = { 'lsp', 'path', 'snippets', 'buffer' },
-              },
-              fuzzy = { implementation = "prefer_rust_with_warning" }
-            })
-          '';
-        }
-        {
-          plugin = oil-nvim; # file explorer
-          config = toLua "require('oil').setup()";
-        }
-        {
-          plugin = nvim-treesitter; # syntax highlighting
-          config = toLua ''
-            require('nvim-treesitter.configs').setup({
-              highlight = {enable = true},
-            })
-          '';
-        }
-        treesitterSyntaxes
-      ];
-    extraLuaConfig = ''
-      ${builtins.readFile ./vim.lua}
+  config = {
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+      plugins = let
+        toLua = str: "lua << EOF\n${str}\nEOF\n";
+      in
+        with pkgs.vimPlugins; [
+          fzf-lua # quick opener w/ fzf
+          nvim-lspconfig # language servers
+          typst-preview-nvim # live preview for typst
+          yazi-nvim # file picker
+          {
+            plugin = blink-cmp; # auto completion
+            config = toLua ''
+              require('blink.cmp').setup({
+                -- C-y to accept
+                -- C-space: Open menu or open docs if already open
+                -- C-n/C-p or Up/Down: Select next/previous item
+                -- C-e: Hide menu
+                -- C-k: Toggle signature help (if signature.enabled = true)
+                keymap = { preset = 'default' },
 
-      -- lsp servers
-      vim.lsp.enable({
-        ${
-        builtins.concatStringsSep ", "
-        (lib.mapAttrsToList (name: _: "'${name}'") languageServers)
-      }
-      })
-    '';
-    extraPackages = let
-      lsp = lib.mapAttrsToList (_: pkg: pkg) languageServers;
-      fmt = with pkgs; [alejandra];
-    in
-      lsp ++ fmt;
+                appearance = {
+                  nerd_font_variant = 'mono'
+                },
+                completion = { documentation = { auto_show = false } },
+                sources = {
+                  default = { 'lsp', 'path', 'snippets', 'buffer' },
+                },
+                fuzzy = { implementation = "prefer_rust_with_warning" }
+              })
+            '';
+          }
+          {
+            plugin = oil-nvim; # file explorer
+            config = toLua "require('oil').setup()";
+          }
+          {
+            plugin = nvim-treesitter; # syntax highlighting
+            config = toLua ''
+              require('nvim-treesitter.configs').setup({
+                highlight = {enable = true},
+              })
+            '';
+          }
+          (nvim-treesitter.withPlugins (_: cfg.treesitterParsers))
+        ];
+      extraLuaConfig = ''
+        ${builtins.readFile ./vim.lua}
+
+        -- lsp servers
+        vim.lsp.enable({
+          ${
+          builtins.concatStringsSep ", "
+          (lib.mapAttrsToList (name: _: "'${name}'") cfg.languageServers)
+        }
+        })
+      '';
+      extraPackages = let
+        lsp = lib.mapAttrsToList (_: pkg: pkg) cfg.languageServers;
+        fmt = with pkgs; [alejandra];
+      in
+        lsp ++ fmt;
+    };
   };
 }
