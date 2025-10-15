8526acdf707e052a102734319a2f66e6f52e2fc6
mawz <mawz@hey.com>
2025-10-14 18:35:54 -0700

    move extra packages out of minimal conf

--- modules/systems/home/default.nix
+++ modules/systems/home/default.nix
@@ -90,6 +90,94 @@
     };
   };
 
+  local.yazi.keybinds = {
+    drag-and-drop = {
+      bind = "<C-n>";
+      command = ''shell -- ${lib.getExe pkgs.xdragon} -x -i -T -a "$@"'';
+    };
+    copy-to-clipboard = {
+      bind = "y";
+      command = [''shell -- for path in "$@"; do echo "file://$path"; done | ${pkgs.wl-clipboard}/bin/wl-copy -t text/uri-list'' "yank"];
+    };
+  };
+
+  # docs:
+  #   - https://github.com/gokcehan/lf/blob/master/doc.md
+  #   - https://github.com/gokcehan/lf/wiki/Tips
+  programs.lf = {
+    enable = true;
+    commands = let
+      copy-cmd = "${pkgs.wl-clipboard}/bin/wl-copy";
+    in {
+      copy-path = ''
+        ''${{
+        ${copy-cmd} $f
+        }}
+      '';
+      copy-name = ''
+        ''${{
+        basename "$f" | ${copy-cmd}
+        }}
+      '';
+      copy-dir = ''
+        ''${{
+        dirname "$f" | ${copy-cmd}
+        }}
+      '';
+      copy-files = ''
+        ''${{
+          echo "$fx" | awk '{ print "file://" $0 }' | ${copy-cmd} -t text/uri-list
+        }}
+      '';
+      dragon-out = ''
+        ''${{
+          readarray -t files <<<"$fx"
+          ${pkgs.xdragon}/bin/xdragon -a -x "''${files[@]}"
+        }}
+      '';
+    };
+    keybindings = {
+      y = null;
+      yy = "copy";
+      yp = "copy-path";
+      yn = "copy-name";
+      yd = "copy-dir";
+      yf = "copy-files";
+      yo = "dragon-out";
+      a = "push %mkdir<space>";
+      gd = "cd /mnt/gdrive";
+      gf = "cd /mnt/super-fly";
+      gm = "cd /mnt/mr-president";
+    };
+    previewer = {
+      keybinding = "i";
+      source = pkgs.writeShellScript "pv.sh" ''
+        #!/bin/sh
+
+        mimeType=$(xdg-mime query filetype "$1")
+        echo "Mime type: $mimeType"
+
+        shopt -s nocasematch
+        case "$mimeType" in
+            video/* | audio/* | image/*)
+                ${pkgs.exiftool}/bin/exiftool -S -DateTimeOriginal -MediaCreateDate -FileModifyDate "$1";
+                echo "--------------------------------";; #${pkgs.exiftool}/bin/exiftool "$1";;
+        esac
+        case "$1" in
+            *.tar*) tar tf "$1";;
+            *.zip) unzip -l "$1";;
+            *.rar) unrar l "$1";;
+            *.7z) 7z l "$1";;
+            *.pdf) pdftotext "$1" -;;
+            # *) highlight -O ansi "$1" || cat "$1";;
+        esac
+
+        less "$1"
+      '';
+    };
+  };
+
+
   # Theme settings
   # needed for stylix theming
   programs.btop.enable = true;
--- modules/systems/home/minimal.nix
+++ modules/systems/home/minimal.nix
@@ -14,11 +14,11 @@ in {
       type = attrsOf (submodule {
         options = {
           bind = mkOption {
-            type = listOf str;
+            type = either str (listOf str);
             description = "Keyboard inputs";
           };
           command = mkOption {
-            type = str;
+            type = either str (listOf str);
             description = "Command to run";
           };
         };
@@ -81,83 +81,6 @@ in {
       '';
     };
 
-    # inspiration:
-    #   - https://github.com/gokcehan/lf/blob/master/doc.md
-    #   - https://github.com/gokcehan/lf/wiki/Tips
-    #   - https://github.com/vimjoyer/lf-nix-video
-    programs.lf = {
-      enable = true;
-      commands = let
-        copy-cmd = "${pkgs.wl-clipboard}/bin/wl-copy";
-      in {
-        copy-path = ''
-          ''${{
-          ${copy-cmd} $f
-          }}
-        '';
-        copy-name = ''
-          ''${{
-          basename "$f" | ${copy-cmd}
-          }}
-        '';
-        copy-dir = ''
-          ''${{
-          dirname "$f" | ${copy-cmd}
-          }}
-        '';
-        copy-files = ''
-          ''${{
-            echo "$fx" | awk '{ print "file://" $0 }' | ${copy-cmd} -t text/uri-list
-          }}
-        '';
-        dragon-out = ''
-          ''${{
-            readarray -t files <<<"$fx"
-            ${pkgs.xdragon}/bin/xdragon -a -x "''${files[@]}"
-          }}
-        '';
-      };
-      keybindings = {
-        y = null;
-        yy = "copy";
-        yp = "copy-path";
-        yn = "copy-name";
-        yd = "copy-dir";
-        yf = "copy-files";
-        yo = "dragon-out";
-        a = "push %mkdir<space>";
-        gd = "cd /mnt/gdrive";
-        gf = "cd /mnt/super-fly";
-        gm = "cd /mnt/mr-president";
-      };
-      previewer = {
-        keybinding = "i";
-        source = pkgs.writeShellScript "pv.sh" ''
-          #!/bin/sh
-
-          mimeType=$(xdg-mime query filetype "$1")
-          echo "Mime type: $mimeType"
-
-          shopt -s nocasematch
-          case "$mimeType" in
-              video/* | audio/* | image/*)
-                  ${pkgs.exiftool}/bin/exiftool -S -DateTimeOriginal -MediaCreateDate -FileModifyDate "$1";
-                  echo "--------------------------------";; #${pkgs.exiftool}/bin/exiftool "$1";;
-          esac
-          case "$1" in
-              *.tar*) tar tf "$1";;
-              *.zip) unzip -l "$1";;
-              *.rar) unrar l "$1";;
-              *.7z) 7z l "$1";;
-              *.pdf) pdftotext "$1" -;;
-              # *) highlight -O ansi "$1" || cat "$1";;
-          esac
-
-          less "$1"
-        '';
-      };
-    };
-
     programs.yazi = {
       enable = true;
 
@@ -202,16 +125,6 @@ in {
               on = "!";
               run = ''shell "$SHELL" --block'';
               desc = "Open shell here";
-            }
-            # drag and drop
-            {
-              on = "<C-n>";
-              run = ''shell -- ${lib.getExe pkgs.xdragon} -x -i -T -a "$@"'';
-            }
-            # copy to system clipboard
-            {
-              on = "y";
-              run = [''shell -- for path in "$@"; do echo "file://$path"; done | ${pkgs.wl-clipboard}/bin/wl-copy -t text/uri-list'' "yank"];
             }
             # shortcuts
             {
