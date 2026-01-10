c0730d379b2f5a9b1a63e2cc84ae3545296c7891
mawz <mawz@hey.com>
2026-01-10 13:25:08 -0800

    reconfigure hypr binds for colemak

--- modules/hyprland/binds.nix
+++ modules/hyprland/binds.nix
@@ -21,7 +21,7 @@
         # openers
         "$mainMod, R, exec, $menu"
         "$mainMod, T, exec, $terminal"
-        "$mainMod, E, exec, $fileManager"
+        "$mainMod, F, exec, $fileManager"
         "$mainMod, B, exec, $browser"
 
         # universal copy paste
@@ -30,8 +30,8 @@
         "$mainMod, V, sendshortcut, , XF86Paste, activewindow"
 
         # notifications
-        "$mainMod, N, exec, ${pkgs.mako}/bin/makoctl dismiss"
-        "$mainMod, I, exec, ${pkgs.mako}/bin/makoctl invoke"
+        "$mainMod, D, exec, ${pkgs.mako}/bin/makoctl dismiss"
+        "$mainMod, K, exec, ${pkgs.mako}/bin/makoctl invoke"
 
         # misc
         "$mainMod, O, movecurrentworkspacetomonitor, +1"
@@ -51,10 +51,10 @@
         "$mainMod ALT, E, exit,"
 
         # Move focus with mainMod + movement keys
-        "$mainMod, H, movefocus, l"
-        "$mainMod, L, movefocus, r"
-        "$mainMod, K, movefocus, u"
-        "$mainMod, J, movefocus, d"
+        "$mainMod, N, movefocus, l"
+        "$mainMod, I, movefocus, r"
+        "$mainMod, U, movefocus, u"
+        "$mainMod, E, movefocus, d"
         "$mainMod, Left,  movefocus, l"
         "$mainMod, Right, movefocus, r"
         "$mainMod, Up,    movefocus, u"
@@ -141,15 +141,15 @@
 
       # groups
       bind = , G, togglegroup,
-      bind = , N, changegroupactive,
+      bind = , C, changegroupactive,
       bind = SHIFT, G, moveoutofgroup,
-      bind = SHIFT, N, changegroupactive, b
+      bind = SHIFT, C, changegroupactive, b
 
       # move rules
-      bind = , H, movewindoworgroup, l
-      bind = , L, movewindoworgroup, r
-      bind = , K, movewindoworgroup, u
-      bind = , J, movewindoworgroup, d
+      bind = , N, movewindoworgroup, l
+      bind = , I, movewindoworgroup, r
+      bind = , U, movewindoworgroup, u
+      bind = , E, movewindoworgroup, d
       bind = , Left,  movewindoworgroup, l
       bind = , Right, movewindoworgroup, r
       bind = , Up,    movewindoworgroup, u
@@ -177,20 +177,20 @@
       bind = SHIFT, 0, movetoworkspace, 10
 
       # resize rules
-      binde = SHIFT, H, resizeactive, -100 0
-      binde = SHIFT, L, resizeactive, 100 0
-      binde = SHIFT, K, resizeactive, 0 -100
-      binde = SHIFT, J, resizeactive, 0 100
+      binde = SHIFT, N, resizeactive, -100 0
+      binde = SHIFT, I, resizeactive, 100 0
+      binde = SHIFT, U, resizeactive, 0 -100
+      binde = SHIFT, E, resizeactive, 0 100
       binde = SHIFT, Left,  resizeactive, -100 0
       binde = SHIFT, Right, resizeactive, 100 0
       binde = SHIFT, Up,    resizeactive, 0 -100
       binde = SHIFT, Down,  resizeactive, 0 100
 
       # escape hatch for switching focus
-      bind = ALT, H, movefocus, l
-      bind = ALT, L, movefocus, r
-      bind = ALT, K, movefocus, u
-      bind = ALT, J, movefocus, d
+      bind = ALT, N, movefocus, l
+      bind = ALT, I, movefocus, r
+      bind = ALT, U, movefocus, u
+      bind = ALT, E, movefocus, d
       bind = ALT, Left,   movefocus, l
       bind = ALT, Right,  movefocus, r
       bind = ALT, Up,     movefocus, u
