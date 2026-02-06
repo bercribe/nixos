565307641c3870dd0617cb8dd9d3eefa20d01cbd
mawz <mawz@hey.com>
2026-02-05 21:22:10 -0800

    treesitter incremental selection

--- modules/systems/home/vim.nix
+++ modules/systems/home/vim.nix
@@ -124,6 +124,13 @@ in {
             config = toLua ''
               require('nvim-treesitter.configs').setup({
                 highlight = {enable = true},
+                incremental_selection = {
+                  enable = true,
+                  keymaps = {
+                    node_incremental = "v",
+                    node_decremental = "V",
+                  },
+                },
               })
             '';
           }
