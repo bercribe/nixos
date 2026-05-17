e65f243682436e05339667981faeeb09f965b25b
mawz <mawz@hey.com>
2026-05-17 15:55:21 -0700

    heavens-door: 810 (current) 2026-05-17 15:52:57  25.11.20260218.6d41bc2  6.12.73

--- modules/systems/common.nix
+++ modules/systems/common.nix
@@ -60,7 +60,7 @@
     LC_NUMERIC = "en_US.UTF-8";
     LC_PAPER = "en_US.UTF-8";
     LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+    LC_TIME = "en_GB.UTF-8"; # start week on Monday
   };
 
   # Configure keymap in X11
